2e301565b692ccc559ee6c2916fb0d14781354d4
Sean Yager <seanyagerart@gmail.com>
2025-07-09 20:38:01 -0500

    card wrangling

--- cards/abstract_card/card_resources/card_detail_base.gd
+++ cards/abstract_card/card_resources/card_detail_base.gd
@@ -8,6 +8,7 @@ enum CARD_TYPE {
 	SPADE,
 	CROSS,
 	WAVE,
+	STAR,
 }
 
 @export var card_name : StringName = ''
@@ -16,6 +17,7 @@ enum CARD_TYPE {
 @export var suit_texture : Texture2D
 @export var card_bar_texture : Texture2D
 
+@export var destroy_effects : Array[Resource]
 @export var card_cost : int = 0
 
 @export_category('Card Stats')
