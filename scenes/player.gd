8e255cfcd2c79cec8ce801c2468a0d4279a5e5b6
Sean Yager <contact@seanyager.com>
2025-07-14 18:18:55 -0400

    handle character movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,8 +1,71 @@
 extends Node2D
-class_name AbstractPlayer
+class_name PlayerContainer
 
-@export var deck : Array[AbstractCardDetails]
+@onready var player: Sprite2D = $PlayerSprite
 
-func _ready():
-	deck.shuffle()
-	var hand = deck.slice(0,3)
+@onready var down: RayCast2D = $Down
+@onready var right: RayCast2D = $Right
+@onready var up: RayCast2D = $Up
+@onready var left: RayCast2D = $Left
+
+var move_distance : int = 16
+var bounce_distance : int = 4
+var lock_movement : bool = false
+
+func unlock_movement():
+	lock_movement = false
+
+func invalid_movement(relative_pos: Vector2):
+	lock_movement = true
+	var origin_pos = position
+	var lvl_tween : Tween = create_tween()
+	lvl_tween.tween_property(self, "position", position + relative_pos, 0.1)
+	lvl_tween.tween_property(self, "position", origin_pos, 0.1)
+	lvl_tween.tween_callback(unlock_movement)
+
+func valid_movement(relative_pos: Vector2):
+	lock_movement = true
+	var pos = player.position
+	var lvl_tween : Tween = create_tween()
+	lvl_tween.tween_property(self, "position", position + relative_pos, 0.2)
+	lvl_tween.tween_callback(unlock_movement)
+	
+func _input(event: InputEvent) -> void:
+	if lock_movement:
+		return
+		
+	var movement_vector : Vector2
+	var valid_move : bool = false
+	
+	if Input.is_action_just_pressed('up'):
+		if up.is_colliding():
+			movement_vector = Vector2(0, -bounce_distance) 
+		else:
+			valid_move = true
+			movement_vector = Vector2(0, -move_distance)
+			
+	if Input.is_action_just_pressed('down'):
+		if down.is_colliding():
+			movement_vector = Vector2(0, bounce_distance) 
+		else:
+			valid_move = true
+			movement_vector = Vector2(0, move_distance)
+	
+	if Input.is_action_just_pressed('left'):
+		if left.is_colliding():
+			movement_vector = Vector2(-bounce_distance, 0) 
+		else: 
+			movement_vector = Vector2(-move_distance, 0)
+			valid_move = true
+		
+	if Input.is_action_just_pressed('right'):
+		if right.is_colliding():
+			movement_vector = Vector2(bounce_distance, 0) 
+		else: 
+			movement_vector = Vector2(move_distance, 0)
+			valid_move = true
+		
+	if valid_move:
+		valid_movement(movement_vector)
+	else:
+		invalid_movement(movement_vector)
