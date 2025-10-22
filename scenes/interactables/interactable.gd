06e2dc1d49af429f89606e9564f4a3bd086c1732
Sean Yager <seanyagerart@gmail.com>
2025-10-21 23:23:31 -0400

    refactor interactables, make barrel, hook up exports for fast creation

--- scenes/interactables/interactable.gd
+++ scenes/interactables/interactable.gd
@@ -1,93 +1,70 @@
+@tool
 extends Node2D
-class_name InteractableNode
-
-@export var inital_interaction_sound : AudioStreamPlayer2D
-@export var secondary_interaction_sound : AudioStreamPlayer2D
-@export var action : Action
-@export var selector_indicator : Sprite2D
-@export var can_become_walkable : bool = false
-@export var oneshot_interaction : bool = false
-
-@onready var interaction_area = $InteractionArea
-@onready var interaction_icon_bg: Sprite2D = $InteractionIconBG
-@onready var interaction_icon = $InteractionIconBG/InteractionIcon
-@onready var interaction_sprite = $InteractionSprite
-@onready var reward_container = $RewardContainer
-
-@export var interactable_card_reward : Action = null
-
-const CARD_COLLECTIBLE = preload("res://cards/card_collectible.tscn")
-
-var can_interact : bool = false
-var hint_icon_visible : bool = false
-
-var has_been_interacted : bool = false
-
-var reward_target_node : CollectibleCard
+class_name Interactable
 
 enum STATES {
 	DEFAULT,
-	HINTED,
-	PREPARED,
-	OPEN,
-	CLOSED,
-	USED,
+	LOOTED,
+	INTERACTED,
+	DESTROYED,
 }
+
+signal new_loot_collected(passed_loot : Action)
+
+@export var action_triggers : Dictionary[Action, STATES]
+@export var interaction_sprites : Dictionary[STATES, CompressedTexture2D]
+@export var interaction_sounds : Dictionary[STATES, AudioStreamRandomizer]
+@export var interaction_reward : Action
+
+@onready var sprite: AnimatedSprite2D = %Sprite
+@onready var sfx: AudioStreamPlayer2D = $SoundEffect
+@onready var interaction_icon: Sprite2D = $InteractionHintBG/InteractionIcon
+@onready var hint_circle: Sprite2D = $HintCircle
 
 var current_state : STATES = STATES.DEFAULT
 
 func _ready():
-	if interactable_card_reward:
-		var card_reward_node = CARD_COLLECTIBLE.instantiate()
-		card_reward_node.card_details = interactable_card_reward
-		reward_container.add_child(card_reward_node)
-	interaction_icon.texture = action.texture
+	EventBus.selected_card_changed.connect(show_icon_hint)
+	EventBus.card_deselected.connect(hide_icon)
 
-func attempt_to_claim_collectable():
-	if reward_container.get_child_count() == 0:
-		return
-	var reward : CollectibleCard = reward_container.get_child(0)
-	if reward and reward.has_method('reveal_self'):
-		reward.reveal_self()
+func show_icon_hint(primed_action: Action):
+	if action_triggers.has(primed_action):
+		interaction_icon.texture = primed_action.texture
+		hint_circle.show()
 
-func initial_interaction_handler():
-	attempt_to_claim_collectable()
-	if can_become_walkable:
-		interaction_area.set_collision_layer_value(1, false)
-	interaction_sprite.frame = 1
-	inital_interaction_sound.play()
-	has_been_interacted = true
+func hide_icon():
+	hint_circle.hide()
+
+func handle_interaction(attempted_action : Action):
+	if action_triggers.has(attempted_action):
+		current_state = action_triggers[attempted_action]
+		match(current_state):
+			STATES.LOOTED:
+				loot_interactable()
+			STATES.INTERACTED:
+				trigger_interaction()
+			STATES.DESTROYED:
+				trigger_destruction()
+				
+func trigger_interaction():
+	sprite.texture = interaction_sprites[STATES.INTERACTED]
+	sfx.stream = interaction_sounds[STATES.INTERACTED]
+	sfx.play()
+	exhaust_action_by_state(STATES.INTERACTED)
+
+func trigger_destruction():
+	sprite.texture = interaction_sprites[STATES.DESTROYED]
+	sfx.stream = interaction_sounds[STATES.DESTROYED]
+	sfx.play()
+	exhaust_action_by_state(STATES.DESTROYED)
 	
-func secondary_interaction_handler():
-	if can_become_walkable:
-		interaction_area.set_collision_layer_value(1, true)
-	interaction_sprite.frame = 0
-	secondary_interaction_sound.play()
-	has_been_interacted = false
+func loot_interactable():
+	sprite.texture = interaction_sprites[STATES.LOOTED]
+	new_loot_collected.emit(interaction_reward)
+	exhaust_action_by_state(STATES.LOOTED)
 
-func attempt_interaction():
-	if not selector_indicator.visible:
-		return
-	get_tree().call_group('player_cards', 'use_card')
-	if not has_been_interacted:
-		initial_interaction_handler()
-	elif has_been_interacted:
-		secondary_interaction_handler()
-
-func update_hint(passedAction : Action):
-	if action == passedAction:
-		show_interaction_icon()
-	else:
-		hide_interaction_icon()
-
-func hide_interaction_icon():
-	interaction_icon_bg.hide()
-	
-func show_interaction_icon():
-	interaction_icon_bg.show()
-
-func show_selector_indicator():
-	selector_indicator.show()
-	
-func hide_selector_indicator():
-	selector_indicator.hide()
+func exhaust_action_by_state(passed_state: STATES):
+	for action : Action in action_triggers:
+		if action_triggers[action] == passed_state:
+			action_triggers.erase(action)
+		
---
+++ scenes/interactables/interactable_node.gd
@@ -0,0 +1,93 @@
+extends Node2D
+class_name InteractableNode
+
+@export var inital_interaction_sound : AudioStreamPlayer2D
+@export var secondary_interaction_sound : AudioStreamPlayer2D
+@export var action : Action
+@export var selector_indicator : Sprite2D
+@export var can_become_walkable : bool = false
+@export var oneshot_interaction : bool = false
+
+@onready var interaction_area = $InteractionArea
+@onready var interaction_icon_bg: Sprite2D = $InteractionIconBG
+@onready var interaction_icon = $InteractionIconBG/InteractionIcon
+@onready var interaction_sprite = $InteractionSprite
+@onready var reward_container = $RewardContainer
+
+@export var interactable_card_reward : Action = null
+
+const CARD_COLLECTIBLE = preload("res://cards/card_collectible.tscn")
+
+var can_interact : bool = false
+var hint_icon_visible : bool = false
+
+var has_been_interacted : bool = false
+
+var reward_target_node : CollectibleCard
+
+enum STATES {
+	DEFAULT,
+	HINTED,
+	PREPARED,
+	OPEN,
+	CLOSED,
+	USED,
+}
+
+var current_state : STATES = STATES.DEFAULT
+
+func _ready():
+	if interactable_card_reward:
+		var card_reward_node = CARD_COLLECTIBLE.instantiate()
+		card_reward_node.card_details = interactable_card_reward
+		reward_container.add_child(card_reward_node)
+	interaction_icon.texture = action.texture
+
+func attempt_to_claim_collectable():
+	if reward_container.get_child_count() == 0:
+		return
+	var reward : CollectibleCard = reward_container.get_child(0)
+	if reward and reward.has_method('reveal_self'):
+		reward.reveal_self()
+
+func initial_interaction_handler():
+	attempt_to_claim_collectable()
+	if can_become_walkable:
+		interaction_area.set_collision_layer_value(1, false)
+	interaction_sprite.frame = 1
+	inital_interaction_sound.play()
+	has_been_interacted = true
+	
+func secondary_interaction_handler():
+	if can_become_walkable:
+		interaction_area.set_collision_layer_value(1, true)
+	interaction_sprite.frame = 0
+	secondary_interaction_sound.play()
+	has_been_interacted = false
+
+func attempt_interaction():
+	if not selector_indicator.visible:
+		return
+	get_tree().call_group('player_cards', 'use_card')
+	if not has_been_interacted:
+		initial_interaction_handler()
+	elif has_been_interacted:
+		secondary_interaction_handler()
+
+func update_hint(passedAction : Action):
+	if action == passedAction:
+		show_interaction_icon()
+	else:
+		hide_interaction_icon()
+
+func hide_interaction_icon():
+	interaction_icon_bg.hide()
+	
+func show_interaction_icon():
+	interaction_icon_bg.show()
+
+func show_selector_indicator():
+	selector_indicator.show()
+	
+func hide_selector_indicator():
+	selector_indicator.hide()
